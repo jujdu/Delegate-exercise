083dc564edd0ce8ddf296edeb08eb2913bf999bb
NewUser <michael.sidoruk@icloud.com>
2019-01-16 01:02:52 +0300

    Initial Commit

---
+++ delegates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  delegates
+//
+//  Created by Michael Sidoruk on 16/01/2019.
+//  Copyright © 2019 Michael Sidoruk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
