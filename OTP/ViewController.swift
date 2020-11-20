19d20ecfd19dcefdc04b6e8f1b19983f937e1712
Michael Sidoruk <michael.sidoruk@waveaccess.com>
2020-11-20 15:42:00 +0300

    Initial Commit

---
+++ OTP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OTP
+//
+//  Created by Michael Sidoruk on 20.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
